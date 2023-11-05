983bf6000ee2eead28a1891cefedfdd80d6c4b20
vanderleipinto <vandecopinto@gmail.com>
2023-11-05 07:33:53 -0300

    Flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,7 @@
 class MainController < ApplicationController
   def index
-    flash[:notice]= "Logged in successfully"
+    flash.now[:notice]= "Logged in successfully"
+    # .now para o flash não persistir no proximo request e vai funcionar somente na página atual
     flash[:alert]= "Invalid email or password"
   end
 end
