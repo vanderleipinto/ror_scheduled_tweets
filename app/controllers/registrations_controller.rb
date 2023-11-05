556e9e4f9cb81e407c4d90aa94a90ef55902ee91
vanderleipinto <vandecopinto@gmail.com>
2023-11-05 17:44:40 -0300

    Finalizando a criação do form. não está reconhecendo o erro de não salvar no BD

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,28 @@
+class RegistrationsController < ApplicationController
+  
+  def new
+    @user = User.new
+  end
+
+
+  def create
+    # params = {"email"=>"mauroinfraero@yahoo.com.br", "password_confirmation"=>"[FILTERED]", "password"=>"[FILTERED]"}, "commit"=>"Create User"}
+    
+    @user = User.new(user_params)
+    
+    if @user.save #if  saves to database correctly
+      redirect_to root_path, notice: "Successfully created account."
+    else      
+      render :new
+      # views/registrations/new.html.erb
+    end
+
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:email,:password,:password_confirmation)
+    # means:  these are the only things we allow you to set
+  end
+end
