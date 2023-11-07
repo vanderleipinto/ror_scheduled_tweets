35f93ade18684e06aea834af53563c3347efe38e
vanderleipinto <vandecopinto@gmail.com>
2023-11-07 19:05:00 -0300

    Login com verificação de password e email. Algumas alterações com :unprocessable_entity e authenticate com password_confirmation.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,24 @@
 class SessionsController < ApplicationController
+
+  def new
+
+  end
+  
+
+  def create
+      user = User.find_by(email: params[:email])
+
+      if user.present? && user.authenticate(params[:password_confirmation])
+        session[:user_id] = user.id
+        redirect_to root_path, notice: "Logged in successfully"
+      else
+        flash[:alert] = "Invalid email or password"
+        render :new, status: :unprocessable_entity
+      end      
+  end
+
+
+
   def destroy
     session[:user_id] = nil
     redirect_to root_path, notice: "Logged out"
