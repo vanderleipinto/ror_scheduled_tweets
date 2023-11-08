8387b9d4d35331f56944bdda83c857ef5b5fed7e
vanderleipinto <vandecopinto@gmail.com>
2023-11-08 17:43:04 -0300

    Criando sessão para current_user, mudando sessão para ApplicationController e criando links para Log in e sign up.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,10 +5,10 @@ class MainController < ApplicationController
     # flash[:alert]= "Invalid email or password"
 
 
-    #autenticação de sessão
-    if session[:user_id]
+    #autenticação de sessão foi removida para application_controller para rodar em todaa aplicação 
+    # if session[:user_id]
       # @user = User.find(session[:user_id]) se o usuário for apagado esse find vai gerar erro e parar o sistem. usaremos o find_by
-      @user = User.find_by(id: session[:user_id])
-    end
+      # @user = User.find_by(id: session[:user_id])
+    # end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   get "about", to: "about#index"
-  
+
+  get "password", to: "password#edit"
   get "sign_up",  to: "registrations#new"
   post "sign_up", to: "registrations#create"
   
